d71f056fb709106601be4d022e793eb08c18e729
LambdaMode <parsayazdani1120@gmail.com>
2025-05-04 15:04:08 +0330

    Commit No.3: specified ProductRepository and injected to ProductServiceImpl

---
+++ src/main/java/com/myproject/simpleonlineshop/service/Impl/ProductServiceImpl.java
@@ -0,0 +1,73 @@
+package com.myproject.simpleonlineshop.service.Impl;
+
+import com.myproject.simpleonlineshop.model.Product;
+import com.myproject.simpleonlineshop.repository.ProductRepository;
+import com.myproject.simpleonlineshop.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public Product UpdateProduct(Product updateProductRequest, Long id) {
+        return null;
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+}
