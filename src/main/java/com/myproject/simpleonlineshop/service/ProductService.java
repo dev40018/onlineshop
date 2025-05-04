c8f9e66b8a4e6ca0ce504c67bbdb9e7d899f8c8f
LambdaMode <parsayazdani1120@gmail.com>
2025-05-04 14:50:30 +0330

    Commit No.2: specified ProductService methods

---
+++ src/main/java/com/myproject/simpleonlineshop/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.myproject.simpleonlineshop.service;
+
+
+import com.myproject.simpleonlineshop.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    Product UpdateProduct(Product updateProductRequest, Long id);
+    Long countProductsByBrandAndName(String brand, String name);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    List<Product> getProductsByName(String name);
+}
