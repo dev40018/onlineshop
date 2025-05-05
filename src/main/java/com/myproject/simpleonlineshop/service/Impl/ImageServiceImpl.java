86acd3b4902a18b376334b074e792bdf2cbf85e9
LambdaMode <parsayazdani1120@gmail.com>
2025-05-05 22:59:22 +0330

    Commit No.31: implemented basic methods

--- src/main/java/com/myproject/simpleonlineshop/service/Impl/ImageServiceImpl.java
+++ src/main/java/com/myproject/simpleonlineshop/service/Impl/ImageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.myproject.simpleonlineshop.service.Impl;
 
+import com.myproject.simpleonlineshop.exception.ResourceNotFoundException;
 import com.myproject.simpleonlineshop.model.Image;
+import com.myproject.simpleonlineshop.repository.ImageRepository;
 import com.myproject.simpleonlineshop.service.ImageService;
+import com.myproject.simpleonlineshop.service.ProductService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,14 +12,26 @@ import java.util.List;
 
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final ImageRepository imageRepository;
+    private final ProductService productService;
+
+    public ImageServiceImpl(ImageRepository imageRepository,
+                            ProductService productService) {
+        this.imageRepository = imageRepository;
+        this.productService = productService;
+    }
+
     @Override
     public Image getImageById(Long id) {
-        return null;
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("no such image exists"));
     }
 
     @Override
     public void deleteImageById(Long id) {
-
+        imageRepository.findById(id)
+                .ifPresentOrElse(imageRepository::delete,
+                        () -> {throw new ResourceNotFoundException("image not found");});
     }
 
     @Override
@@ -26,6 +41,5 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public Image updateImage(MultipartFile file, Long imageId) {
-        return null;
     }
 }
