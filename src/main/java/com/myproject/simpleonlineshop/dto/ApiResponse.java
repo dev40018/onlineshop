0e3b135f91a1e30c1ee16a00cf40ae3d0573e4bd
LambdaMode <parsayazdani1120@gmail.com>
2025-05-06 14:16:31 +0330

    Commit No.38: adding attributes to ApiResponse

--- src/main/java/com/myproject/simpleonlineshop/dto/ApiResponse.java
+++ src/main/java/com/myproject/simpleonlineshop/dto/ApiResponse.java
@@ -1,4 +1,11 @@
 package com.myproject.simpleonlineshop.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiResponse {
+    private String message;
+    private Object data;
 }
