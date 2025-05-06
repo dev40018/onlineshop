31b2d3c194d7e37a28f37b402f4a830cebfb979a
LambdaMode <parsayazdani1120@gmail.com>
2025-05-06 11:14:38 +0330

    Commit No.33: create ImageDto for a Response Object to user

---
+++ src/main/java/com/myproject/simpleonlineshop/dto/ImageDto.java
@@ -0,0 +1,11 @@
+package com.myproject.simpleonlineshop.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUlr;
+}
