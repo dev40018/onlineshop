9deca247849ed93c253678d7b64332bdd54c76bd
LambdaMode <parsayazdani1120@gmail.com>
2025-05-07 11:05:10 +0330

    Commit No.44: CategoryController: implementing GET  Endpoint for accessing all categories

--- src/main/java/com/myproject/simpleonlineshop/controller/CategoryController.java
+++ src/main/java/com/myproject/simpleonlineshop/controller/CategoryController.java
@@ -1,4 +1,35 @@
 package com.myproject.simpleonlineshop.controller;
 
+
+import com.myproject.simpleonlineshop.dto.ApiResponse;
+import com.myproject.simpleonlineshop.model.Category;
+import com.myproject.simpleonlineshop.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error:", HttpStatus.INTERNAL_SERVER_ERROR));
+        }
+    }
+
 }
