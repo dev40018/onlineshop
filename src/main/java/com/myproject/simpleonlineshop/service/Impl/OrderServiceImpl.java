20b17996498c1005565effb0f80dc30eb48d2365
LambdaMode <parsayazdani1120@gmail.com>
2025-05-20 12:25:51 +0330

    Commit No.78: getOrder is implemented

--- src/main/java/com/myproject/simpleonlineshop/service/Impl/OrderServiceImpl.java
+++ src/main/java/com/myproject/simpleonlineshop/service/Impl/OrderServiceImpl.java
@@ -1,11 +1,19 @@
 package com.myproject.simpleonlineshop.service.Impl;
 
+import com.myproject.simpleonlineshop.exception.ResourceNotFoundException;
 import com.myproject.simpleonlineshop.model.Order;
+import com.myproject.simpleonlineshop.repository.OrderRepository;
 import com.myproject.simpleonlineshop.service.OrderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     public Order placeOrder(Long userId) {
         return null;
@@ -13,7 +21,10 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order getOrder(Long orderId) {
-        return null;
+        return orderRepository.findById(orderId)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("No such order Exists")
+                );
     }
 
 }
