07b21271320427d4e04a84ee2a83af5d8981b62a
LambdaMode <parsayazdani1120@gmail.com>
2025-05-30 17:43:10 +0330

    Commit No.105: adding Role entity

---
+++ src/main/java/com/myproject/simpleonlineshop/model/Role.java
@@ -0,0 +1,33 @@
+package com.myproject.simpleonlineshop.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "t_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JoinTable()
+    private Collection<User> users = new HashSet<>();
+
+
+    public Role(String name){
+        this.name = name;
+    }
+}
