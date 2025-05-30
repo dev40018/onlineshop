c3e9c2438a71f921d9368a3de771635dffabb89a
LambdaMode <parsayazdani1120@gmail.com>
2025-05-30 18:37:20 +0330

    Commit No.105: adding relationship between User and Role with user_roles table

--- src/main/java/com/myproject/simpleonlineshop/model/User.java
+++ src/main/java/com/myproject/simpleonlineshop/model/User.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 @Entity
@@ -27,6 +29,18 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // if you care about history of orders, don't use CascadeType.REMOVE
     private List<Order> orders;
+
+    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.PERSIST, CascadeType.REFRESH})
+    //Detaching removes the entity from the current persistence context (e.g., EntityManager), and changes won't be tracked.
+    //When you refresh the parent entity (reload from DB), all child entities are also refreshed.
+    //Useful if you suspect data is stale or changed in the database.
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), // id field of  User Table
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id") // this id is field of Role table
+    )
+    private Collection<Role> roles = new HashSet<>();
 }
