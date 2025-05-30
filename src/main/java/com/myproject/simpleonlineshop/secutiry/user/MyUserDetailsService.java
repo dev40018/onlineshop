0c2eaec218e5335adc87f8409a9ae207a4afecae
LambdaMode <parsayazdani1120@gmail.com>
2025-05-30 19:34:11 +0330

    Commit No.106: implementing MyUserDetailsService method to fetch User and then if exists, create a UserDetails Object from it

--- src/main/java/com/myproject/simpleonlineshop/secutiry/user/MyUserDetailsService.java
+++ src/main/java/com/myproject/simpleonlineshop/secutiry/user/MyUserDetailsService.java
@@ -1,12 +1,30 @@
 package com.myproject.simpleonlineshop.secutiry.user;
 
+import com.myproject.simpleonlineshop.exception.ResourceNotFoundException;
+import com.myproject.simpleonlineshop.model.User;
+import com.myproject.simpleonlineshop.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // first we check if user exists by email
+        User user = Optional.ofNullable(userRepository.findByEmail(email)).orElseThrow(() -> new UsernameNotFoundException("No Such User Found in Database"));
+        // if exits we create a userDetails from
+        MyUserDetails.buildUserDetails(user);
         return null;
     }
 }
